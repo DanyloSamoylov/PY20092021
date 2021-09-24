21c1bb6ddbb0e32756bb884c16f0908a02f2186b
DanyloSamoylov <danylosamoylov@gmail.com>
2021-09-24 15:46:37 +0300

    my first commit

---
+++ 1_1_2.py
@@ -0,0 +1,10 @@
+# just print Hello world
+print('Hello world')
+
+# answer Hello with using TAB and step on new line
+print('Hello', end='\t!!!\n')
+
+# comments from Big Man Tyrone (Youtube)
+print('Every 60 seconds', 'minute passes', sep=' - ', end=' :)\n')
+print('Every 60 minutes', ' an hour passes', sep=',', end='!\n')
+print('The Earth is flat', end='!!!\n')
