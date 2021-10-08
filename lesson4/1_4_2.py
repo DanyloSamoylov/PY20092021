f8c26fff94537f1c770dbce7bc8858323f96e030
DanyloSamoylov <danylosamoylov@gmail.com>
2021-10-08 22:18:27 +0300

    my 10 commit .

--- lesson4/1_4_2.py
+++ lesson4/1_4_2.py
@@ -4,17 +4,16 @@ import random
 list_1 = []
 list_2 = []
 i = 0
-while i < 10:                              # lets add random numbers to list_1
-    random_number = random.randint(1, 10)
-    list_1.append(random_number)
-    i += 1
+while i < 20:
+    if i < 10:
+        random_number = random.randint(1, 10)
+        list_1.append(random_number)
+        i += 1
+    else:
+        random_number = random.randint(1, 10)
+        list_2.append(random_number)
+        i += 1
 print('List 1:', list_1, type(list_1))
-i = 0
-while i < 10:                              #  lets add random numbers to list_2
-    random_number = random.randint(1, 10)
-    list_2.append(random_number)
-    i += 1
 print('List 2:', list_2, type(list_2))
-
-list_3 = set(list_1) | set(list_2)
+list_3 = set(list_1) & set(list_2)
 print('List 3:', list_3, type(list_3))
