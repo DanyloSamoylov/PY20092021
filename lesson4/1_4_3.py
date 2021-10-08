9241043ac3076a8511749f1a0c5b43c8442126f4
DanyloSamoylov <danylosamoylov@gmail.com>
2021-10-08 22:25:14 +0300

    my 10 commit .

--- lesson4/1_4_3.py
+++ lesson4/1_4_3.py
@@ -1,8 +1,8 @@
-x = list(range(1, 101))
+list_1 = list(range(1, 101))
+list_2 = []
 i = 0
-new = []
-while i < x.index(100):
+while i < list_1.index(100):
     if i % 7 == 0 and i % 5 != 0:
-        new.append(i)
+        list_2.append(i)
     i += 1
-print(new)
+print(list_2)
